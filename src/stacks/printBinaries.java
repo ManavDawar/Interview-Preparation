612b2f7aef091781619ed10937174a4898fb0184
Manav Dawar <manavdawar5@gmail.com>
2019-06-06 20:15:05 +0530

    Stacks Ques

--- src/stacks/printBinaries.java
+++ src/stacks/printBinaries.java
@@ -1,9 +1,52 @@
 package stacks;
 
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Scanner;
+
 public class printBinaries {
 
+	
+	public static class Helper{
+		String bin;
+		int data;
+		
+		Helper(int data,String bin){
+			this.data=data;
+			this.bin=bin;
+		}
+		
+	}
+	public static void getBinary(int n) {
+
+		LinkedList<Helper> queue = new LinkedList<>();
+		Helper p = new Helper(1,"1");
+		
+		queue.add(p);
+		
+		while(queue.size()>0) {
+			
+			Helper rem = queue.remove();
+			System.out.print(rem.bin+" ");
+			
+			Helper left = new Helper(2*rem.data,rem.bin+"0");
+			Helper right = new Helper(2*rem.data+1,rem.bin+"1");
+			
+			if(left.data<=n) {
+				queue.addLast(left);
+			}
+			if(right.data<=n) {
+				queue.addLast(right);
+			}
+		}
+		
+	}
+
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+
+		Scanner scn = new Scanner(System.in);
+		int n = scn.nextInt();
+		getBinary(n);
 
 	}
 
