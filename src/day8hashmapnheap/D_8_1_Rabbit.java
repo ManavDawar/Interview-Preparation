8e98d3ce5676a7e77053e9b70a4c537199399278
Manav Dawar <manavdawar5@gmail.com>
2019-06-15 15:24:55 +0530

    Day 8 Hashmap and Heap

--- src/day8hashmapnheap/D_8_1_Rabbit.java
+++ src/day8hashmapnheap/D_8_1_Rabbit.java
@@ -1,10 +1,33 @@
 package day8hashmapnheap;
 
+import java.util.HashMap;
+
 public class D_8_1_Rabbit {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static int count(int[] arr) {
+		int count = 0;
+		HashMap<Integer, Integer> map = new HashMap<>();
+		for (int i = 0; i < arr.length; i++) {
+			if (map.containsKey(arr[i])) {
+				map.put(arr[i], map.get(arr[i]) + 1);
+			} else {
+				map.put(arr[i], 1);
+			}
+		}
 
+		for (int key : map.keySet()) {
+			if (key + 1 >= map.get(key)) {
+				count += key + 1;
+			} else {
+				count += (key + 1) * Math.ceil(map.get(key) * 1.0 / (key + 1));
+			}
+		}
+		return count;
+	}
+
+	public static void main(String[] args) {
+		int[] arr = { 2, 2, 2, 2, 4, 4, 4, 6, 6, 6, 3, 3, 3, 3, 3, 3, 7, 7 };
+		System.out.println(count(arr));
 	}
 
 }
